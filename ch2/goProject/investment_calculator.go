b52fd452d0e6acd6e1384bab41869faaabc9bf0a
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-01 12:11:22 +0300

    wip

--- ch2/goProject/investment_calculator.go
+++ ch2/goProject/investment_calculator.go
@@ -6,12 +6,29 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+  const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
 
-	var futrueValue = investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
-	fmt.Println(futrueValue)
+  fmt.Print("investment Amount: ")
+  fmt.Scan(&investmentAmount)
+
+  fmt.Print("Expected Return Rate: ")
+  fmt.Scan(&expectedReturnRate)
+
+  fmt.Print("Years: ")
+  fmt.Scan(&years)
+
+	futrueValue := investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
+  futureRealValue := futrueValue / math.Pow(1+inflationRate/100,years)
+
+  formattedFV := fmt.Sprintf("Future Value: %.4f\n", futrueValue)
+  formattedRFV := fmt.Sprintf("Future Value (adjusted for Inflation): %f\n", futureRealValue)
+  /* fmt.Println("Future Value:", futrueValue) */
+  /* fmt.Printf("Future Value: %.4f\nFuture Value (adjusted for Inflation): %f\n", futrueValue, futureRealValue) */
+	/* fmt.Println("Future Value (adjusted for Inflation):", futureRealValue) */
+  fmt.Print(formattedFV, formattedRFV)
 }
 
 func Add(a int, b int) {
