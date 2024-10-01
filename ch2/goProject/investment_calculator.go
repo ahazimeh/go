9fb91c48febc55adc76c09276f6c4815f4428058
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-01 09:16:11 +0300

    wip

--- ch2/goProject/investment_calculator.go
+++ ch2/goProject/investment_calculator.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	var investmentAmount float64 = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futrueValue = investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
+	fmt.Println(futrueValue)
 }
 
 func Add(a int, b int) {
