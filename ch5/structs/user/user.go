293f58da6f30bca5a234c371c298a50d7d47efca
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-02 13:58:39 +0300

    ch5

--- ch5/structs/user/user.go
+++ ch5/structs/user/user.go
@@ -13,6 +13,12 @@ type User struct {
 	createdAt time.Time
 }
 
+type Admin struct {
+	email    string
+	password string
+	User
+}
+
 func (u User) OutputUserDetails() {
 	fmt.Println(u.firstName, u.lastName, u.birthdate)
 }
@@ -26,15 +32,28 @@ func (u *User) ClearUserName() {
 	u.lastName = ""
 }
 
+func NewAdmin(email, password string) Admin {
+	return Admin{
+		email:    email,
+		password: password,
+		User: User{
+			firstName: "ADMIN",
+			lastName:  "ADMIN",
+			birthdate: "---",
+			createdAt: time.Now(),
+		},
+	}
+}
+
 func New(firstName, lastName, birthdate string) (*User, error) {
-  if firstName == "" || lastName == "" || birthdate == "" {
-    return nil, errors.New("first name, last name and birthdate are required.")
-  }
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birthdate are required.")
+	}
 
 	return &User{
 		firstName: firstName,
 		lastName:  lastName,
 		birthdate: birthdate,
 		createdAt: time.Now(),
-	},nil
+	}, nil
 }
