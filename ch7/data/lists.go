b7a83e3e49b64fdbb7e563f16917899644c4f236
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-02 19:53:57 +0300

    wip

--- ch7/data/lists.go
+++ ch7/data/lists.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
   prices := []float64{10.99, 8.99} // this syntax create a slice and go create a slice bhind the scenes
-  fmt.Println(prices[1])
+  fmt.Println(prices[0:1])
+  prices[1]=9.99
 
-  prices = append(prices, 5.99) // go will create a new array and add this element to the new array
+  prices = append(prices, 5.99, 12.99,29.99,100.10) // go will create a new array and add this element to the new array
   prices = prices[1:] // removes the first element
   fmt.Println(prices)
+
+  discountPrices := []float64{100.10, 80.99, 20.59}
+  prices = append(prices, discountPrices...)
 }
 
 /* func main() {
