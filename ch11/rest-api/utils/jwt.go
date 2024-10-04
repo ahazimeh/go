8cadaa3bf64beeab1cdc036125f63599df67ee84
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-04 14:13:19 +0300

    wip

--- ch11/rest-api/utils/jwt.go
+++ ch11/rest-api/utils/jwt.go
@@ -9,44 +9,44 @@ import (
 
 const secretKey = "supersecret"
 
-func GenerateToken(email string, userId int64) (string, error){
-  token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-    "email": email,
-    "userId": userId,
-    "exp": time.Now().Add(time.Hour * 2).Unix(),
-  })
+func GenerateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 2).Unix(),
+	})
 
-  return token.SignedString([]byte(secretKey))
+	return token.SignedString([]byte(secretKey))
 }
 
-func verifyToken(token string) error {
-  parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error){
-    _, ok := token.Method.(*jwt.SigningMethodHMAC)
+func VerifyToken(token string) (int64, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 
-    if !ok {
-      return nil, errors.New("Unexpected signing method")
-    }
+		if !ok {
+			return nil, errors.New("Unexpected signing method")
+		}
 
-    return secretKey, nil
-  })
+		return []byte(secretKey), nil
+	})
 
-  if err !=nil {
-    return errors.New("Could not parse token.")
-  }
+	if err != nil {
+		return 0, errors.New("Could not parse token.")
+	}
 
-  tokenIsValid := parsedToken.Valid
+	tokenIsValid := parsedToken.Valid
 
-  if !tokenIsValid {
-    return errors.New("Invalid token!")
-  }
+	if !tokenIsValid {
+		return 0, errors.New("Invalid token!")
+	}
 
-  // claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 
-  /* if !ok {
-    return errors.New("Invalid token claims.")
-  } */
+	if !ok {
+		return 0, errors.New("Invalid token claims.")
+	}
 
-  // email := claims["email"].(string)
-  // userId := claims["userId"].(int64)
-  return nil
+	// email := claims["email"].(string)
+	userId := int64(claims["userId"].(float64))
+	return userId, nil
 }
