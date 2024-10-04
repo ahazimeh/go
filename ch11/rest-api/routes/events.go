d3e1a31a8f4651238c444386ec3368231d017b37
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-04 14:43:19 +0300

    wip

--- ch11/rest-api/routes/events.go
+++ ch11/rest-api/routes/events.go
@@ -62,10 +62,16 @@ func updateEvent(context *gin.Context) {
     return;
   }
 
-  _,err = models.GetEventById(eventId)
+  userId := context.GetInt64("userId")
+  event,err := models.GetEventById(eventId)
 
   if err != nil {
     context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+    return
+  }
+
+  if event.UserID != userId {
+    context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
     return
   }
 
@@ -93,10 +99,17 @@ func deleteEvent(context *gin.Context) {
     return;
   }
 
+  userId := context.GetInt64("userId")
   event,err := models.GetEventById(eventId)
+
 
   if err != nil {
     context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+    return
+  }
+
+  if event.UserID != userId {
+    context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete event"})
     return
   }
 
