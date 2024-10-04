7b9609b88318a18a28e6ec75216e003ae7a8e7aa
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-04 10:35:47 +0300

    wip

--- ch11/rest-api/main.go
+++ ch11/rest-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/rest-api/db"
 	"example.com/rest-api/models"
@@ -14,7 +15,7 @@ func main() {
 
   server.GET("/events", getEvents)
   server.POST("/events", createEvent)
-
+  server.GET("/events/:id", getEvent)
   server.Run(":8080")
 }
 
@@ -25,6 +26,21 @@ func getEvents(context *gin.Context) {
     return
   }
   context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+  eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+  if err != nil {
+    context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+    return;
+  }
+  event, err := models.GetEventById(eventId)
+  if err != nil {
+    context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+    return;
+  }
+
+  context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
