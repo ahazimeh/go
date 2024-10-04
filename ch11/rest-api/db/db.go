ebf42ca8fc09ba583d62af5003bea31dd1a7160b
Ali Hazimeh <hazimeh95@gmail.com>
2024-10-04 07:32:58 +0300

    wip

--- ch11/rest-api/db/db.go
+++ ch11/rest-api/db/db.go
@@ -18,5 +18,24 @@ func InitDB() {
 	DB.SetMaxOpenConns(10) // how many connections can be opened simultaneously
 	DB.SetMaxIdleConns(5) // how many connections we want to keep open if no one is using these connections at the moment
 
-	/* createTables() */
+	createTables()
+}
+
+func createTables() {
+  createEventsTable := `
+  CREATE TABLE IF NOT EXISTS events (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  location TEXT NOT NULL,
+  datetime DATETIME NOT NULL,
+  user_id INTEGER
+  )
+  `
+
+  _, err := DB.Exec(createEventsTable)
+
+  if err != nil {
+    panic("Could not create events table.")
+  }
 }
